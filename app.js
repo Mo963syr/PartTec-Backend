5f92fc3896c410fdc1974803bb41dfd184293a4f
Mo963syr <moafaqaqeed01@gmail.com>
2026-02-24 01:22:29 +0300

    feat: add CORS support to handle cross-origin requests

--- app.js
+++ app.js
@@ -49,6 +49,17 @@ api.use('/payment', paymentRoutes);
 app.use('/parttec', api);
 
 const cors = require('cors');
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin) res.setHeader('Access-Control-Allow-Origin', origin);
+  res.setHeader('Vary', 'Origin');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') return res.status(204).end();
+  next();
+});
 
 app.use(cors({
   origin: [
