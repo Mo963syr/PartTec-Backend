1180551a653ef0ce96235e5fc95a003776f00daf
Mo963syr <moafaqaqeed01@gmail.com>
2026-09-25 17:56:03 +0300

    fix: remove unnecessary blank line at the beginning of app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -30,6 +29,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/parttec/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(
   cors({
